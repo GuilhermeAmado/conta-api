d53e55edcf9eb15689850e8aac308935e688526d
Guilherme Amado <guiii@live.com>
2019-08-01 14:29:50 -0300

    Update teste repositório cliente

--- src/test/java/com/db1/conta/contaapi/repository/ClienteRepositoryTest.java
+++ src/test/java/com/db1/conta/contaapi/repository/ClienteRepositoryTest.java
@@ -4,6 +4,7 @@ package com.db1.conta.contaapi.repository;
 import com.db1.conta.contaapi.domain.entity.*;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -30,13 +31,22 @@ public class ClienteRepositoryTest {
     @Autowired
     private AgenciaRepository agenciaRepository;
 
+    @Before
+    public void settUp() {
+        enderecoRepository.deleteAll();
+        cidadeRepository.deleteAll();
+        clienteRepository.deleteAll();           // deleta tudo ao inicio do teste
+        agenciaRepository.deleteAll();
+        contaRepository.deleteAll();
+    }
+
     @After
     public void afterTest() {
-        clienteRepository.deleteAll();           // deleta tudo ao final do teste
         enderecoRepository.deleteAll();
-        contaRepository.deleteAll();
         cidadeRepository.deleteAll();
+        clienteRepository.deleteAll();           // deleta tudo ao final do teste
         agenciaRepository.deleteAll();
+        contaRepository.deleteAll();
     }
 
     @Test
@@ -51,6 +61,7 @@ public class ClienteRepositoryTest {
         Endereco enderecoSalvo = enderecoRepository.save(endereco);
 
         Assert.assertEquals(cliente.getNome(), clienteSalvo.getNome());
+        Assert.assertEquals(cliente.getListaEnderecos(), clienteSalvo.getListaEnderecos());
 
     }
 }
