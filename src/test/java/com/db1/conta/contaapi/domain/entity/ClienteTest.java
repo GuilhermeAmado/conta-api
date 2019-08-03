db510fe83f4f773ef265d2c8d0e3515f0e043310
Guilherme Amado <guiii@live.com>
2019-08-03 18:32:10 -0300

    Update testes da classe Cliente

--- src/test/java/com/db1/conta/contaapi/domain/entity/ClienteTest.java
+++ src/test/java/com/db1/conta/contaapi/domain/entity/ClienteTest.java
@@ -2,20 +2,12 @@ package com.db1.conta.contaapi.domain.entity;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class ClienteTest {
 
     @Test
     public void deveRetornarExceptionQuandoNomeForNull() {
         String mensagem = null;
-        List<Endereco> endereco = new ArrayList<Endereco>();
-        List<Conta> conta = new ArrayList<Conta>();
         try {
             Cliente cliente = new Cliente(null, "12345678901");
         } catch (Exception e) {
@@ -23,6 +15,45 @@ public class ClienteTest {
         }
 
         Assert.assertEquals(Cliente.INFORMAR_NOME_OBRIGATORIO, mensagem);
+    }
+
+    @Test
+    public void deveRetornarExceptionQuandoCpfForNull() {
+        String mensagem = null;
+        try {
+            Cliente cliente = new Cliente("Guilherme", null);
+        } catch (Exception e) {
+            mensagem = e.getMessage();
+        }
+
+        Assert.assertEquals(Cliente.INFORMAR_CPF_OBRIGATORIO, mensagem);
+    }
+
+    @Test
+    public void deveRetornarExceptionQuandoCpfForInvalido() {
+        String mensagem = null;
+        try {
+            Cliente cliente = new Cliente("Guilherme", "123");
+        } catch (Exception e) {
+            mensagem = e.getMessage();
+        }
+
+        Assert.assertEquals(Cliente.CPF_INVALIDO, mensagem);
+    }
+
+    @Test
+    public void deveCriarUmCliente() {
+        String mensagem = null;
+        Cliente cliente = null;
+        try {
+            cliente = new Cliente("Guilherme", "12345678901");
+        } catch (Exception e) {
+            mensagem = e.getMessage();
+        }
+
+        Assert.assertNull(mensagem);
+        Assert.assertEquals("Guilherme", cliente.getNome());
+        Assert.assertEquals("12345678901", cliente.getCpf());
     }
 
 }
